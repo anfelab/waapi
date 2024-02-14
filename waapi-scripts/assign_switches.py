c61cf8fbc989f8dd32c83128d939cc860ba695c5
anfelab <anfelab@hotmail.com>
2024-02-15 00:26:15 +0900

    Script working with minor errors

--- waapi-scripts/assign_switches.py
+++ waapi-scripts/assign_switches.py
@@ -1,101 +1,134 @@
-from wwise_helpers import *
+from waapi import WaapiClient, CannotConnectToWaapiException
+from wwise_helpers import show_error_message, show_message, show_success_message
 
 #Set the WAAPI port
-waapi_port = set_waapi_port()
-def get_switch_group():
-    selected_items = get_selected_items()
-    for item in selected_items:
-        id = item[0]
-        try:
-            with WaapiClient(waapi_port) as client:
-                args= {
-                    "from":{"id":[id]},
-                    "options":{"return":["id", "name","SwitchGroupOrStateGroup"]}
-                }
-                result = client.call("ak.wwise.core.object.get", args)
-                print(result)
-                switch_group_ids = []
-                switch_group_ids.append(result["return"][0]['SwitchGroupOrStateGroup']["id"])
-                switch_group_names = []
-                switch_group_names.append(result["return"][0]['SwitchGroupOrStateGroup']["name"])
+def set_client(ip = "127.0.0.1", port = 8080):
+    waapi_port = f"ws://{ip}:{port}/waapi"
+    print(f"Client set to: {waapi_port}")
+    client = WaapiClient(waapi_port)
+    return client
 
-                if not switch_group_ids:
-                    show_error_message("No switch container selected or no SwitchGroup assigned!")
-            return switch_group_ids, switch_group_names
+def get_selected_items(client):
+    selected_items = []
+    args = {"options": {"return":["id", "name","SwitchGroupOrStateGroup"]}}
+    result = client.call("ak.wwise.ui.getSelectedObjects", args)
+    for item in result["objects"]:
+        selected_items.append(item)
+    return selected_items
 
-        except CannotConnectToWaapiException:
-            show_error_message("Could not connect to Wwise Authoring API.")
+def get_children(client, container_id):
+    args = {
+        "from": {"id": [container_id]},
+        "transform":[{"select":["children"]}],
+        "options": {"return": ["name", "id"]}
+    }
+    result = client.call("ak.wwise.core.object.get", args)
+    if result:
+        return result["return"]
 
-def get_switches(switch_group_ids):
-    switch_id_list = []
-    switch_name_list = []
-    for switch_group_id in switch_group_ids:
-        try:
-            with WaapiClient(waapi_port) as client:
-                # Fetch switches within the Switch Group
-                args = {
-                    "from": {"id": [switch_group_id]},
-                    "transform": [{"select": ['children']}],
-                    "options": {"return": ["id", "name", "@SwitchGroupOrStateGroup"]}
-                }
-                result = client.call("ak.wwise.core.object.get", args)
-                switches = result.get('return', [])
-                print(switches)
-                
-                if not switches:
-                    show_error_message(f"No switches found for Switch Group ID: {switch_group_id}")
-                    continue
-                for switch in switches:
-                    switch_id = switch['id']
-                    switch_name = switch["name"]
-                    switch_id_list.append(switch_id)
-                    switch_name_list.append(switch_name)
-        except CannotConnectToWaapiException:
-            show_error_message("Could not connect to Wwise Authoring API.")
-    client.disconnect()
-    return switch_id_list, switch_name_list
+def check_if_container_is_assigned(client,container_id,switch_list):  #(client, container_id,container_name,switch_list)
+    unassaigned_switches = []
+    assigned_switches = []
+    args={
+        "id":container_id,
+    }
+    result = client.call("ak.wwise.core.switchContainer.getAssignments",args)
+    if result:
+        for assingment in result["return"]:
+            assigned_switches.append(assingment["stateOrSwitch"])
+    else:
+        print(f"No assignments found")
 
-def get_assignments(container_id, switch_id):
-    with WaapiClient(waapi_port) as client:
-        children_args = {
-                "from": {"id": [container_id]},
-                "transform": [{"select": ['children']}],
-                "options": {"return": ["id", "name", "@SwitchOrState"]}
-            }
-        assignments = client.call("ak.wwise.core.object.get", children_args)
-        print(assignments)
+    print(switch_list)
+    unassaigned_switches = remove_values(assigned_switches,switch_list)
+    print(unassaigned_switches, len(unassaigned_switches))
+    return unassaigned_switches
 
-        if not assignments:
-        # If no assignments found, create a random container and assign it
-            create_and_assign_container(client, switch_id)
-        else:
-            print(f"Switch {switch_id} already has assignments, skipping.")
+def remove_values(list1, list2):
+    return[item for item in list2 if item["id"] not in list1]
 
-def create_and_assign_container(switch_id, container_id):
-    # Implementation for creating a random container and assigning it to the switch
-    # Placeholder for creating a random container
-    container_name = switch_id  #Example container name, adjust as needed
-    with WaapiClient(waapi_port) as client:
-        container_args = {
-            "parent": container_id,
-            "type": "RandomSequenceContainer",  # or any other container type
-            "name": container_name
-        }
-        create_result = client.call("ak.wwise.core.object.create", container_args)
-        if create_result:
-            print(f"Created random container {container_name} and assigned to switch {switch_id}")
-        else:
-            print(f"Failed to create random container for switch {switch_id}")
+def get_switches(client, switch_group_id):
+    switch_list = []
+    args = {
+        "from": {"id": [switch_group_id]},
+        "transform": [{"select": ['children']}],
+        "options": {"return": ["id", "name"]}
+    }
+    result = client.call("ak.wwise.core.object.get", args)
+    switches = result["return"]
+    for switch in switches:
+        switch_list.append(switch)
+    
+    if not switches:
+        show_error_message(f"No switches found for Switch Group ID: {switch_group_id}")
+    return switch_list
+
+def create_container(client,container_id,container_name,switch_name, switch_id):
+    objects_created = 0
+    new_container_name = f"{switch_name.lower()}"
+    container_args = {
+        "parent": container_id,
+        "type": "RandomSequenceContainer",  # or any other container type
+        "name": new_container_name,
+        "onNameConflict": "merge"
+    }
+    create_result = client.call("ak.wwise.core.object.create", container_args)
+    new_cont_id = create_result["id"]
+    if create_result:
+        print(f"Created Random Container {new_container_name} and assigned to switch {switch_name}")
+        assign_switch(client, new_cont_id,switch_id)
+        objects_created += 1
+        
+    else:
+        show_error_message(f"Failed to create Random Container for Switch {switch_name}")
+    return objects_created
+
+def assign_switch(client, child_id, switch_id):
+    args = {
+        "child":child_id,
+        "stateOrSwitch":switch_id
+    }
+    client.call("ak.wwise.core.switchContainer.addAssignment",args)
 
 def main():
-        selected_items = get_selected_items_type("id")
-        print(selected_items)
-        for container_id in selected_items:
-            switch_group_id, switch_group_name = get_switch_group()
-            print(f"Switch group is {switch_group_id},{switch_group_name}")
-            switch_id_list, switch_name_list = get_switches(switch_group_id)
-            for switch_id in switch_id_list:
-                print(switch_id)
+    try:
+        client = set_client()
+        with client:
+            selected_items = get_selected_items(client)
+            for item in selected_items:
+                container_id = item["id"] #Get the ID for every container selected
+                container_name = item["name"] #Get the name for every container selected
+                switch_group_id = item["SwitchGroupOrStateGroup"]["id"] #Get the assigned Group Switch ID
+                switch_group_name = item["SwitchGroupOrStateGroup"]["name"] #Get the assigned Group Switch Name
+                if not switch_group_id or not switch_group_name:
+                    show_error_message("No Switch Container selected or no switch group assigned!")
+                    client.disconnect()
+                current_children = get_children(client, container_id) #Look for preexisting containers
+                switch_list = get_switches(client, switch_group_id)
+                check_if_container_is_assigned(client, container_id,switch_list)
+
+                if current_children:
+                    for current_child in current_children:
+                        current_child_id = current_child["id"]
+                        unnasigned_switches = check_if_container_is_assigned(client,current_child_id,switch_list)
+                        for unnasigned_switch in unnasigned_switches:
+                            unnasigned_switch_id = unnasigned_switch["id"]
+                            unnasigned_switch_name = unnasigned_switch["name"]
+                            create_result = create_container(client,container_id,container_name,unnasigned_switch_name,unnasigned_switch_id)              
+                    show_success_message(f"Succesfully assigned containers for all switches!")
+                else:
+                    switch_list = get_switches(client, switch_group_id)
+                    unnasigned_switches = switch_list
+                    for unnasigned_switch in unnasigned_switches:
+                        unnasigned_switch_id = unnasigned_switch["id"]
+                        unnasigned_switch_name = unnasigned_switch["name"]
+                        create_result = create_container(client,container_id,container_name,unnasigned_switch_name,unnasigned_switch_id)
+                    show_success_message(f"Succesfully assigned containers for all switches!")
+
+            client.disconnect()
+             
+    except CannotConnectToWaapiException:
+        show_error_message("Could not connect to Waapi: Is Wwise running and Wwise Authoring API enabled?")
     
 if __name__ == "__main__":
     main()
